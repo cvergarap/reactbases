c3bcb21e0392bff6f40be486b92b045c71ff21a8
Cristyan Vergara <cvergara@veritran.com>
2022-07-02 19:01:13 -0500

    Se crean botones para contador, incrementar y decrementar

---
+++ src/components/Counter.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+function Counter(){
+    const [count, setCount] = useState(0);
+    return(
+        <div 
+            className= "mb-5">
+                <h1 
+                    className= "mb-5 text-3xl">
+                        Contador
+                </h1>
+
+            <button 
+                onClick= {()=>setCount(count + 1)}
+                className="btn bg-gray-500 hover:bg-gray-700 ml-2 mr-1 text-white-2xl py-2 px-4 rounded">
+                    Incrementar [+]
+            </button>
+            <button 
+                onClick= {()=>setCount(count - 1)}
+                className="btn bg-gray-500 hover:bg-gray-700 ml-1 mr-2 text-white-2xl py-2 px-4 rounded">
+                    Decrementar [-]
+            </button>
+            <br/>
+            <p id="idContador" className="mt-2 text-2xl text-white">{count}</p>
+        </div>
+    )
+}
+
+export default Counter;
